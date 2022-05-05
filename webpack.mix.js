157139fddfcc2316ea1aa58b91e736cb2ffba021
Ryan McDonagh <laslomcd@gmail.com>
2022-05-05 10:53:25 -0400

    added template

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix');
 
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,7 +12,13 @@ const mix = require('laravel-mix');
  |
  */
 
+
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/main.scss', 'public/css')
+    .options({
+        postCss: [
+            require('postcss-custom-properties')
+        ]
+    })
+
+
